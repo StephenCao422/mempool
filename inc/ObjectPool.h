717a694a4226491d9bf4157c5d94675a9ca9892e
Stephen Cao <cao48@illinois.edu>
2025-08-07 17:31:14 -0400

    recycle free blk and add sizeof(T)<sizeof(void*) edge case

--- inc/ObjectPool.h
+++ inc/ObjectPool.h
@@ -16,31 +16,33 @@ class ObjectPool {
 
             T* obj = nullptr;
 
-            if(_remaneBytes < sizeof(T)){
-                _remanetBytes = 128*1024;
-                _memory = (char*)malloc(_remanetBytes);
-                if(_memory==nullptr){
-                    throw std::bad_alloc();
-                }
+            if(_freelist != nullptr) {
+                void* next = *(void**)_freelist;//hold addr of 2nd blk
+                obj = (T*)_freelist;
+                _freelist = next; // pop from freelist
             }
+            else {
+                if(_remaneBytes < sizeof(T)){
+                    _remanetBytes = 128*1024;
+                    _memory = (char*)malloc(_remanetBytes);
+                    if(_memory==nullptr){
+                        throw std::bad_alloc();
+                    }
+                }
 
-            obj = (T*)_memory;
-            _memory += sizeof(T);
-            _remanetBytes -= sizeof(T);
+                obj = (T*)_memory;
+                size_t objSize = sizeof(T)<sizeof(void*) ? sizeof(void*) : size(T);
+                _memory += objSize;
+                _remanetBytes -= objSize;
+            }
 
             return obj;
         }
 
-        void* delete(T* obj){
+        void* delete(T* obj){ //head insert
 
-            if(_freelist ==nullptr){ //freelist is empty
-                *(void**)obj = nullptr; //*(void**) get ptr size in diff sys
-                _freelist = obj;
-            }
-            else{ //freelist not empty, head insert
-                *(void**) obj = _freelist;
-                _freelist = obj;
-            }
+            *(void**)obj = _freelist;
+            _freelist = obj;
         }
 
 };
