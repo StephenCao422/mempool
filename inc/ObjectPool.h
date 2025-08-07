bf36b1892dbabbfd8036adf575fc5614701f3c19
Stephen Cao <cao48@illinois.edu>
2025-08-07 12:06:15 -0400

    add new() interface

--- inc/ObjectPool.h
+++ inc/ObjectPool.h
@@ -3,3 +3,27 @@
 #include <iostream>
 using std::cout;
 using std::endl;
+
+template <class T>
+class ObjectPool {
+    private:
+        char* _memory = nullptr;
+        void* _freelist = nullptr;
+
+    public:
+        T* new(){
+            T* obj = nullptr;
+
+            if(_memory==nullptr){
+                _memory = (char*)malloc(128*1024);
+                if(_memory==nullptr){
+                    throw std::bad_alloc();
+                }
+            }
+            obj = (T*)_memory;
+            _memory += sizeof(T);
+
+            return obj;
+        }
+
+};
