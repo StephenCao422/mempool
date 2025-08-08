1cd27b94808fce0af828dc63baea3977362632fb
Stephen Cao <cao48@illinois.edu>
2025-08-08 17:02:29 -0400

    build index in hash table

--- inc/ThreadCache.h
+++ inc/ThreadCache.h
@@ -19,14 +19,9 @@ class SizeClass
 public:
 
     static size_t _RoundUp(size_t size, size_t alignNum){
-        size_t res = 0;
-        if(size % alignNum != 0){
-            res = (size / alignNum +1)*alignNum;
-        } else{
-            res = size;
-        }
-        return res;
+        return ((size+alignNum-1) & ~(alignNum-1)); // efficient use binary
     }
+
 
     static size_t RoundUp(size_t size){
         if(size <= 128){
@@ -48,4 +43,40 @@ public:
             return -1;
         }
     }
+
+
+    static inline size_t _Index(size_t size, size_t align_shift){
+        return ((size + (1 << align_shift) - 1) >> align_shift) - 1;
+    }
+    
+
+    static inline size_t Index(size_t size)
+    {
+        assert(size <= MAX_BYTES);
+
+        static int group_array[4] = { 16, 56, 56, 56 };
+
+        if (size <= 128){
+            return _Index(size, 3);
+        }
+        else if (size <= 1024){
+            return _Index(size - 128, 4) + group_array[0];
+        }
+        else if (size <= 8 * 1024){
+            return _Index(size - 1024, 7) + group_array[1] + group_array[0];
+        }
+        else if (size <= 64 * 1024){
+            return _Index(size - 8 * 1024, 10) + group_array[2] + group_array[1]
+                + group_array[0];
+        }
+        else if (size <= 256 * 1024){
+            return _Index(size - 64 * 1024, 13) + group_array[3] + group_array[2] + group_array[1] + group_array[0];
+        }
+        else{
+            assert(false);
+        }
+        return -1;
+}
+
+
 };
